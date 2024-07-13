25a57ec051dfb543af3c7ad13df10ef6ccbab83f
Mohammad Behnam <mohammadbehnam.dev@gmail.com>
2024-07-13 08:31:57 +0330

    add material ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Button from "@mui/material/Button";
 export default function Home() {
-	return <h1>hello</h1>;
+	return (
+		<>
+			<h1>hello</h1>
+			<Button variant="contained">Hello world</Button>
+		</>
+	);
 }
